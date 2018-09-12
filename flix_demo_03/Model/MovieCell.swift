02fc5e4c15d6c4ee37fc75d581b07c7fc97f27a7
Calvin Thang <calvin@Calvins-MacBook-Pro.local>
2018-09-11 23:14:37 -0400

    ready for submission but missing a few things

---
+++ flix_demo_03/Model/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flix_demo_03
+//
+//  Created by Calvin Thang on 9/8/18.
+//  Copyright © 2018 Calvin Thang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
