1cb253aabfd357b204830d8768c258db7d32539b
Calvin Thang <calvin@Calvins-MacBook-Pro.local>
2018-09-11 23:28:00 -0400

    ready for submission

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 **Flix App** is a movies app using the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
 
-Time spent: **1** hours spent in total
+Time spent: **4** hours spent in total
 
 ## User Stories
 
 The following **required** user stories are complete:
 
 - [x] User sees app icon in home screen and styled launch screen (+1pt)
-- [ ] User can scroll through a list of movies currently playing in theaters from The Movie DB API (+5pt)
-- [ ] User can "Pull to refresh" the movie list (+2pt)
+- [ x] User can scroll through a list of movies currently playing in theaters from The Movie DB API (+5pt)
+- [x ] User can "Pull to refresh" the movie list (+2pt)
 - [ ] User sees a loading state while waiting for the movies to load (+2pt)
 
 The following **stretch** user stories are implemented:
@@ -28,15 +28,15 @@ The following **additional** user stories are implemented:
 
 Please list two areas of the assignment you'd like to **discuss further with your peers** during the next class (examples include better ways to implement something, how to extend your app in certain ways, etc):
 
-1.
-2.
+1.How to change background?
+2.How to make the app more interactive
 
 ## Video Walkthrough
 
 Here's a walkthrough of implemented user stories:
 
 <img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
-
+LICECAP DOESNT WORK
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
 
 ## Notes
--- flix_demo_03/Controller/NowPlayingViewController.swift
+++ flix_demo_03/Controller/NowPlayingViewController.swift
@@ -7,22 +7,39 @@
 //
 
 import UIKit
+import AlamofireImage
+
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+
+    
+    var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         
         tableView.dataSource = self
+        fetchMovies()
         
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
         
+        fetchMovies()
         
-        // Do any additional setup after loading the view.
-        
+    }
+    
+    func fetchMovies() {
+        activityIndicator.startAnimating()
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
         let request = URLRequest (url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -36,25 +53,38 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 print(error.localizedDescription)
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                print(dataDictionary)
                 
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 
-                for movie in movies {
-                    let title = movie["title"] as! String
-                    print(title)
-                }
+                self.movies = movies
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
             }
         }
+        activityIndicator.stopAnimating()
         task.resume()
     }
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.overviewLabel.text = overview
+        cell.titleLabel.text = title
+        
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        let posterURL = URL(string: baseURLString + posterPathString)!
+        cell.posterImageView.af_setImage(withURL: posterURL)
+        
+        
         return cell
     }
     
@@ -63,6 +93,9 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
     
 
     /*
