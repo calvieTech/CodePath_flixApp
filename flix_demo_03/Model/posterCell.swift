d68c779a3723dadd9bacb397881c21e46f318746
Calvin Thang <calvin@Calvins-MacBook-Pro.local>
2018-09-17 18:53:59 -0400

    finished collection view setup

---
+++ flix_demo_03/Model/posterCell.swift
@@ -0,0 +1,14 @@
+//
+//  posterCell.swift
+//  flix_demo_03
+//
+//  Created by Calvin Thang on 9/17/18.
+//  Copyright © 2018 Calvin Thang. All rights reserved.
+//
+
+import UIKit
+
+class posterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
